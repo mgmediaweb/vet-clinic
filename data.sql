3bfc9e2051cd098cc92c7969b3827747aeb78b38
Gonzalo <mgmediaweb@gmail.com>
2022-08-09 12:15:14 -0700

    make a queries for multiples tables

--- data.sql
+++ data.sql
@@ -25,41 +25,84 @@ INSERT INTO animals (
 INSERT INTO animals (
     name, date_of_birth, escape_attempts, neutered, weight_kg
 ) VALUES (
-    'Charmander', '2020-02-08', 0, false, -11
+    'Charmander', '2020-02-08', 0, 0, -11
 );
 
 INSERT INTO animals (
     name, date_of_birth, escape_attempts, neutered, weight_kg
 ) VALUES (
-    'Plantmon', '2021-11-15', 2, true, -5.7
+    'Plantmon', '2021-11-15', 2, 1, -5.7
 );
 
 INSERT INTO animals (
     name, date_of_birth, escape_attempts, neutered, weight_kg
 ) VALUES (
-    'Squirtle', '1993-04-02', 3, false, -12.13
+    'Squirtle', '1993-04-02', 3, 0, -12.13
 );
 
 INSERT INTO animals (
     name, date_of_birth, escape_attempts, neutered, weight_kg
 ) VALUES (
-    'Angemon', '2005-06-12', 1, true, -45
+    'Angemon', '2005-06-12', 1, 1, -45
 );
 
 INSERT INTO animals (
     name, date_of_birth, escape_attempts, neutered, weight_kg
 ) VALUES (
-    'Boarmon', '2005-06-07', 7, true, 20.4
+    'Boarmon', '2005-06-07', 7, 1, 20.4
 );
 
 INSERT INTO animals (
     name, date_of_birth, escape_attempts, neutered, weight_kg
 ) VALUES (
-    'Blossom', '1998-10-13', 3, true, 17
+    'Blossom', '1998-10-13', 3, 1, 17
 );
 
 INSERT INTO animals (
     name, date_of_birth, escape_attempts, neutered, weight_kg
 ) VALUES (
-    'Ditto', '2022-05-14', 4, true, 22
-);  
+    'Ditto', '2022-05-14', 4, 1, 22
+);
+
+/* INSERT OWNERS INTO YOUR TABLE */
+
+INSERT INTO owners (
+    full_name, age
+) VALUES (
+    'Sam Smith', 34
+);
+
+INSERT INTO owners (
+    full_name, age
+) VALUES (
+    'Jennifer Orwell', 19
+);
+
+INSERT INTO owners (
+    full_name, age
+) VALUES (
+    'Bob', 45
+);
+
+INSERT INTO owners (
+    full_name, age
+) VALUES (
+    'Melody Pond', 77
+);
+
+INSERT INTO owners (
+    full_name, age
+) VALUES (
+    'Dean Winchester', 14
+);
+
+INSERT INTO owners (
+    full_name, age
+) VALUES (
+    'Jodie Whittaker', 38
+);
+
+/* INSERT SPECIES INTO YOUR TABLE */
+
+INSERT INTO species (name) VALUES ('Pokemon');
+INSERT INTO species (name) VALUES ('Digimon');
--- queries.sql
+++ queries.sql
@@ -106,3 +106,93 @@ SELECT species, AVG(escape_attempts) as AVERAGE_ESCAPES
     FROM animals 
     WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' 
     GROUP BY species;
+
+/* UPDATE ESPECIES ID */
+
+UPDATE animals
+	SET species_id=1
+	WHERE name LIKE '%mon';
+
+UPDATE animals
+	SET species_id=2
+	WHERE name NOT LIKE '%mon'; 
+
+/* UPDATE OWNER ID */
+
+UPDATE animals
+	SET owner_id=1
+	WHERE name = 'Agumon';
+
+UPDATE animals
+	SET owner_id=2
+	WHERE name IN ('Gabumon', 'Pikachu');
+
+UPDATE animals
+	SET owner_id=3
+	WHERE name IN ('Devimon', 'Plantmon');
+
+UPDATE animals
+	SET owner_id=4
+	WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animals
+	SET owner_id=5
+	WHERE name IN ('Angemon', 'Boarmon');
+
+/* What animals belong to Melody Pond */
+
+SELECT
+	a.name
+    FROM animals a
+    INNER JOIN owners o ON a.owner_id = o.id
+    WHERE o.full_name = 'Melody Pond';
+
+/* List of all animals that are pokemon (their type is Pokemon) */
+
+SELECT
+	a.name
+    FROM animals a
+    INNER JOIN species s ON a.species_id = s.id
+    WHERE s.name = 'Pokemon';
+
+/* List all owners and their animals, remember to include those that don't own any animal. */
+
+SELECT
+	o.full_name, a.name
+    FROM owners o
+    LEFT JOIN animals a ON o.id = a.owner_id;
+
+/* How many animals are there per species? */
+
+SELECT
+	s.name, COUNT(a.name) AS count_species
+    FROM animals a
+    JOIN species s ON a.species_id = s.id
+    GROUP BY s.name;
+
+/* List all Digimon owned by Jennifer Orwell. */
+
+SELECT
+	a.name
+    FROM animals a
+    LEFT JOIN species s ON a.species_id = s.id
+    LEFT JOIN owners o ON a.owner_id = o.id
+    WHERE o.full_name = 'Jennifer Orwell' AND s.name = 'Digimon';
+
+/* List all animals owned by Dean Winchester that haven't tried to escape. */
+
+SELECT
+	a.name
+    FROM animals a
+    LEFT JOIN owners o ON a.owner_id = o.id
+    WHERE o.full_name = 'Dean Winchester' AND a.escape_attempts = 0;
+
+/* Who owns the most animals? */
+
+SELECT
+	o.full_name, COUNT(a.*) AS quantity
+    FROM animals a
+    LEFT JOIN owners o ON a.owner_id = o.id
+    GROUP BY o.full_name
+    ORDER BY quantity DESC
+    LIMIT 1;
