9aa322fdf897c4b8c23ae03bfc029c24b37873da
Gonzalo <mgmediaweb@gmail.com>
2022-08-16 19:11:01 -0700

    fix suggestion from CR

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -12,29 +12,46 @@ CREATE TABLE treatments  (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE medical_history (
+CREATE TABLE medical_histories (
     id SERIAL NOT NULL,
     admitted_at TIMESTAMP,
-    patient_id INT REFERENCES patients(id),
+    patient_id INT,
     status VARCHAR(20),
     PRIMARY KEY (id)
 );
+
+ALTER TABLE medical_histories ADD FOREIGN KEY (patient_id) REFERENCES patients(id);
 
 CREATE TABLE invoices  (
     id SERIAL NOT NULL,
     total_amount DECIMAL(6,3),
     generate_at TIMESTAMP,
     payed_at TIMESTAMP,
-    medical_history_id INT REFERENCES medical_history(id), 
+    medical_history_id INT, 
     PRIMARY KEY (id)
 );
 
+ALTER TABLE invoices ADD FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id); 
+
 CREATE TABLE invoice_items (
     id SERIAL NOT NULL,
-    unique_price DECIMAL(6,2),
+    unit_price DECIMAL(6,2),
     quantity INT,
     total_price DECIMAL(6,2),
-    invoice_id INT REFERENCES invoices(id),
-    treatment_id INT REFERENCES treatments(id),
+    invoice_id INT,
+    treatment_id INT,
     PRIMARY KEY (id)
 );
+
+ALTER TABLE medical_histories ADD FOREIGN KEY (invoice_id) REFERENCES invoices(id);
+ALTER TABLE medical_histories ADD FOREIGN KEY (treatment_id) REFERENCES treatments(id);
+
+CREATE TABLE medical_histories_treatments (
+    id SERIAL NOT NULL,
+    medical_history_id INT,
+    treatment_id INT,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE medical_histories_treatments ADD FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id);
+ALTER TABLE medical_histories_treatments ADD FOREIGN KEY (treatment_id) REFERENCES treatments(id);
